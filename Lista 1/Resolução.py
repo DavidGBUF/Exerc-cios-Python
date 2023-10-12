2d6222254315c9b6e08446576799819ecafc932e
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-10-12 20:12:14 -0300

    Exercícios da Internet

---
+++ Lista 1/Resolução.py
@@ -0,0 +1,58 @@
+# #Questão 1 ===================================
+# print('Olá, Mundo!')
+
+# #Questão 2 ===================================
+# nome= input("Olá! Qual é vosso nome? ")
+# print(f'Olá {nome}! Espero que nós tenhamos uma boa relação!')
+
+
+# #Questão 3 ===================================
+
+# def converter_celsius_para_F(temp):
+#     return (temp*1.8)+32
+
+# temp = float(input("Quantos graus celsius para Fahrenheit? "))
+
+# print('A temperatura em fahrenheit é: ',converter_celsius_para_F(temp),"°")
+
+# #Questão 4 ====================================
+# def media_notas(*notas):
+#     return sum(notas)/len(notas)
+
+# print("A média das notas passadas é: ",media_notas(6,8,8.5))
+
+
+# #Questão 5 ====================================
+# def verificar_primo(num):
+#     if num < 2:
+#         return False
+#     else:
+#         for i in range(2, num):
+#             if num%i==0:
+#                 return False
+#     return True
+
+# print(verificar_primo(67))
+
+
+# #Questão 6 ====================================
+
+# def fatorial(n):
+#     if n == 0:
+#         return 1
+#     return n* fatorial(n-1)
+
+# print(fatorial(5))
+
+
+# #Questão 7 ====================================
+
+# def par_or_impar(num):
+#     if num%2==0:
+#         return "Par"
+#     return "Impar"
+
+# print(par_or_impar(5))
+
+        
+    
---
+++ Lista 2/Resolução.py
@@ -0,0 +1,37 @@
+# #Questão 1 ============================
+# for i in range(1,11):
+#     print(i,end= "  ")
+    
+    
+# #Questão 2 ============================
+# print(sum(range(1,101)))
+
+# #Questão 3 ============================
+
+# numero_1= float(input("Primeiro numero: "))
+# numero_2= float(input("Segundo numero: "))
+
+# print(numero_1*numero_2)
+
+
+# #Questão 4 ============================
+# for i in range(2,51,2):
+#     print(i,end=', ')
+
+
+
+# #Questão 5 ============================
+# def verificar_sinal(num):
+#     if num>0:
+#         return "É positivo"
+#     elif num==0:
+#         return '0 é neutro'
+#     return 'É negativo'
+
+# print(verificar_sinal(-5))
+
+
+# #Questão 6 ============================
+# palavra_reversa= (input('Digite sua palavra: '))[::-1]
+# print(f'Ordem Inversa: {palavra_reversa}')
+
