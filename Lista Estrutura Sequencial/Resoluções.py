7dc93cc6c86c2bab65022065ea63d5c2f3d0dd48
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-10-15 10:26:15 -0300

    Atualização

---
+++ Lista Estrutura Sequencial/Resoluções.py
@@ -0,0 +1,64 @@
+# #Questão1===============================
+# print("Alô Mundo!")
+
+
+
+# #Questão2===============================
+# num=input("Digite um número: ")
+# print(f"O número informado foi {num}")
+
+
+
+# #Questão3===============================
+
+# num1, num2= float(input("Digite dois números e receba sua soma: ")),float(input())
+# print(f"A soma de {num1} e {num2} é igual a {num1+num2}")
+
+
+
+# # Questão4===============================
+# cont=0
+# for i in range(4):
+    
+#     cont+=float(input(f"Digite a {i+1}° Nota: "))
+    
+# print("A média das notas dadas é", cont/4)
+
+
+
+# # Questão5===============================
+# centi=float(input("Digite quantos Centímetros deseja converter: "))
+# print(f"Isso equivale a {centi/100} metros")
+
+
+
+
+# # Questão6===============================
+# raio=float(input("Quantas unidades tem o raio do círculo?: "))
+# print(f"Um circulo com {raio} unidades de medida tem área de {3.141592*raio**2:0.2f} Unidades²")
+
+
+
+
+# # Questão7===============================
+# lado=float(input("Quanto mede o lado de seu Quadrado?: "))
+
+# print(f"A área dele é de {lado**2}u² e o dobro de sua área é de {(lado**2)*2}u²")
+
+
+
+# # Questão8===============================
+# salario_hora,horas_mes= float(input("Quantos reais você ganha por hora?: ")),float(input("E quantas horas você trabalhou este mês?: "))
+# print(f"Seu sálario deve ser de R${salario_hora*horas_mes}")
+
+
+
+# # Questão9===============================
+# fahrenheit=float(input("Converta de Fahrenheit para graus Celsius! Digite a temperatura em °F: "))
+# print(f'Isso equivale a {5 * ((fahrenheit-32) / 9):0.2f}°C')
+
+
+
+
+
+    
