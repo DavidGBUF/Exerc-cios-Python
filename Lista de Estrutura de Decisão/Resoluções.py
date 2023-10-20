8f0599509fc5f66f02ea4b6664c9cebf732ba344
DavidGBUF <129506782+DavidGBUF@users.noreply.github.com>
2023-10-20 14:52:25 -0300

    Nova lista

---
+++ Lista de Estrutura de Decisão/Resoluções.py
@@ -0,0 +1,11 @@
+# #Questão1============================
+# num1, num2=input("Digite 2 números que eu direi o maior: ").split()
+# num1 = float(num1)
+# num2= float(num2)
+# if num1>num2:
+#     print(num1, "é o maior valor")
+# elif num2>num1:
+#     print(num2, "é o maior valor")
+
+# else:
+#     print("Os valores são iguais")
